9ad271d04bd360c762def938dec9a6131f58017a
Outvi V <olvi.git@mailnull.com>
2018-12-13 20:25:41 +0800

    Finish option page

--- options.js
+++ options.js
@@ -3,8 +3,22 @@ var Setting = ext.Setting;
 var searchs = JSON.parse(Setting.get('searchs'));
 
 document.getElementById('btnSave').addEventListener('click', () => {
-  ext.saveSearchs(JSON.parse(document.getElementById('taArea').value));
-  ext.updateSearch();
+  let result = {}, error = false;
+  try {
+    result = JSON.parse(document.getElementById('taArea').value);
+  } catch (err) {
+    error = true;
+    document.getElementById('resultArea').innerText = err;
+  }
+  if (!error) {
+    ext.saveSearchs(result);
+    document.getElementById('resultArea').innerText = "Success.";
+    ext.updateSearch();
+  }
+});
+
+document.getElementById('btnReload').addEventListener('click', () => {
+  document.getElementById('taArea').value = JSON.stringify(searchs);
 });
 
 document.getElementById('taArea').value = JSON.stringify(searchs);
